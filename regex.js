f220694ed0f259aee70263cf61f2b0fa87eca85f
Aakash Pahuja <aakash.pahuja@kony.com>
2018-07-29 02:27:37 +0530

    Add basic wildcard support

--- regex.js
+++ regex.js
@@ -16,9 +16,34 @@ function isEquals (pattern, text) {
 
 function match(pattern, text) {
   if (pattern === "") {
-    return true // Our base case - if the pattern is empty, any inputted text is a match
+    return true
+  } else if (pattern === "$" && text === "") {
+    return true
+  } else if (pattern[1] === "?") {
+    return matchQuestion(pattern, text)
+  } else if (pattern[1] === "*") {
+    return matchStar(pattern, text)
   }
 
   return isEquals(pattern[0], text[0])
   && match(pattern.slice(1), text.slice(1))
+}
+
+function search(pattern, text) {
+  if (pattern[0] === "^") {
+    return match(pattern.slice(1), text)
+  }
+
+  return match(".*" + pattern, text)
+}
+
+function matchQuestion(pattern, text) {
+  return (
+    (isEquals(pattern[0], text[0]) && match(pattern.slice(2), text.slice(1))) ||
+    match(pattern.slice(2), text)
+  );
+}
+
+function matchStar(pattern, text) {
+  return (isEquals(pattern[0], text[0]) && match(pattern, text.slice(1))) || match(pattern.slice(2), text);
 }
